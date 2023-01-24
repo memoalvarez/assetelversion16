800bd63433dcaa9bc148bd07b71271e8100c6f2b
memoalvarez <memoalvarez713@gmail.com>
2023-01-24 10:21:56 -0600

    [FIX] Correcciones en empresarial group

--- empresarial_group/models/sale_subscription.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, api, fields
-
-class SaleSubscription(models.Model):
-    _inherit = 'sale.subscription'
-
-    empresarial_group = fields.Many2one('empresarial.group', string='Grupo empresarial')
-
