f26936ba66b479ecf4e6a007bca8e1ae5d03841c
memoalvarez <memoalvarez713@gmail.com>
2023-01-27 13:19:45 -0600

    [FIX] Correcciones en installed_services 02

--- installed_services/models/__init__.py
+++ installed_services/models/__init__.py
@@ -5,7 +5,7 @@ from . import helpdesk_ticket
 from . import installed_services
 from . import product_product
 from . import res_partner
-from . import sale_subscription_line
+from . import sale_order_line
 from . import sale_subscription
 from . import service_information_cloud
 from . import service_information
