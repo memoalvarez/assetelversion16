0a9f1597ef36975c2a36e9f6d9f186fa61167de0
memoalvarez <memoalvarez713@gmail.com>
2023-02-16 10:30:18 -0600

    [FIX] Correcciones en service_installation 22

--- assetel_partners/models/sign_send_request.py
+++ assetel_partners/models/sign_send_request.py
@@ -23,21 +23,56 @@ class SignSendRequest(models.TransientModel):
             res['afiliacion_id'] = self.env.context.get('afiliacion_id')
             
         return res
-
-
-    def create_request(self, send=True, without_mail=False):
+        
+                
+        
+    def create_request(self):
         template_id = self.template_id.id
         if self.signers_count:
-            signers = [{'partner_id': signer.partner_id.id, 'role': signer.role_id.id} for signer in self.signer_ids]
+            signers = [{'partner_id': signer.partner_id.id, 'role_id': signer.role_id.id, 'mail_sent_order': signer.mail_sent_order} for signer in self.signer_ids]
         else:
-            signers = [{'partner_id': self.signer_id.id, 'role': False}]
-        followers = self.cc_partner_ids.ids
+            signers = [{'partner_id': self.signer_id.id, 'role_id': self.env.ref('sign.sign_item_role_default').id, 'mail_sent_order': self.signer_ids.mail_sent_order}]
+        cc_partner_ids = self.cc_partner_ids.ids
         reference = self.filename
         subject = self.subject
         message = self.message
-
+        message_cc = self.message_cc
+        attachment_ids = self.attachment_ids
+        refusal_allowed = self.refusal_allowed
+        
         if self.afiliacion_id:
             afiliacion_id = self.afiliacion_id.id
-            return self.env['sign.request'].initialize_new_afilation(template_id, signers, followers, reference, afiliacion_id, subject, message, send, without_mail)
+            sign_request = self.env['sign.request'].create({
+                'template_id': template_id,
+                'afiliacion_id': afiliacion_id,
+                'request_item_ids': [Command.create({
+                    'partner_id': signer['partner_id'],
+                    'role_id': signer['role_id'],
+                    'mail_sent_order': signer['mail_sent_order'],
+                }) for signer in signers],
+                'reference': reference,
+                'subject': subject,
+                'message': message,
+                'message_cc': message_cc,
+                'attachment_ids': [Command.set(attachment_ids.ids)],
+                'refusal_allowed': refusal_allowed,
+            })
+            sign_request.message_subscribe(partner_ids=cc_partner_ids)
+            return sign_request
         else:
-            return self.env['sign.request'].initialize_new(template_id, signers, followers, reference, subject, message, send, without_mail)
+            sign_request = self.env['sign.request'].create({
+                'template_id': template_id,
+                'request_item_ids': [Command.create({
+                    'partner_id': signer['partner_id'],
+                    'role_id': signer['role_id'],
+                    'mail_sent_order': signer['mail_sent_order'],
+                }) for signer in signers],
+                'reference': reference,
+                'subject': subject,
+                'message': message,
+                'message_cc': message_cc,
+                'attachment_ids': [Command.set(attachment_ids.ids)],
+                'refusal_allowed': refusal_allowed,
+            })
+            sign_request.message_subscribe(partner_ids=cc_partner_ids)
+            return sign_request
