b5a9d8dbe7f0aa8d33a4b3c17b1ca8848c76ce03
memoalvarez <memoalvarez713@gmail.com>
2023-02-15 22:05:25 -0600

    [FIX] Correcciones en service_installation 21

--- service_installation/models/sale_order_line.py
+++ service_installation/models/sale_order_line.py
@@ -10,7 +10,7 @@ class SaleOrderLine(models.Model):
     
     empresarial_group = fields.Many2one('empresarial.group', string='Grupo empresarial')
 
-
+    #FUNCIONA EN VERSION 16
     @api.onchange('product_template_id', 'product_uom_qty')
     def _onchange_partner_id_service_installation(self):
         if self.order_id.empresarial_group:
